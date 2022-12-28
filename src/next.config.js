42bc0ca2e39a771d931a1282b2474db51aab17d1
bagherani <mohammad.bagherani@gmail.com>
2022-12-28 11:54:10 +0400

    static path map added to the next config

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
 }
 
 module.exports = nextConfig
