ac1fcb2f1f8335bcfac86b4fcdcb9c064786efab
bagherani <mohammad.bagherani@gmail.com>
2023-09-29 20:08:20 +0200

    google analytics added. header changed

---
+++ src/app/components/GoogleAnalytics.tsx
@@ -0,0 +1,21 @@
+import Script from "next/script";
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        async
+        src={"https://www.googletagmanager.com/gtag/js?id=G-SHHLFJ6CEB"}
+      />
+
+      <Script>
+        {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', 'G-SHHLFJ6CEB');
+        `}
+      </Script>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "./components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MOHI BAGHERANI",
-  description: "MOHI BAGHERANI PORTFOLIO",
+  title: "Mohi Bagherani",
+  description: "Mohammad Bagherani's personal website",
 };
 
 export default function RootLayout({
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <GoogleAnalytics />
+      </body>
     </html>
   );
 }
